9398fea9721b154109a1a5a0c633eb43beef118c
mmicr001 <mmicr001@odu.edu>
2016-05-03 16:56:59 -0400

    Added files via upload
    all subjects added. still need to be updated when we get all the data

--- Matlab Analysis/Main.m
+++ Matlab Analysis/Main.m
@@ -2,7 +2,7 @@
 clear; clc; close all;
 
 % Dataset Root Location
-mRootDir = 'C:\Users\Johnson\Google Drive\EVMS\';
+mRootDir = 'C:\Users\Mike\Google Drive\EVMS\';
 
 % 4 possible sessions for each subject:
 % 1: Baseline
@@ -20,99 +20,183 @@ mRootDir = 'C:\Users\Johnson\Google Drive\EVMS\';
 % is. Also some subjects didn't get all the sessions in
 
 % Patient Identification
-Data(1).pID = {'AMa'};
-Data(1).Sessions=[1 2 3 4];
-Data(1).Outcomes.Days = [  1 16 23 30 37 45 55 65 ];
-Data(1).Outcomes.BDI =  [ 40 24 14 15 16 14 16 15 ];
-Data(1).Outcomes.BAI =  [ 14 10  3  3  5  4  4  3 ];
-Data(1).Outcomes.EEGDay = [ 6 14 28 65 ];
+kk=1;
 
-Data(2).pID = {'AWa'};
-Data(2).Sessions=[1 2 3 4];
-Data(2).Outcomes.Days = [  1 45 53 59 66 73 81 93 ];
-Data(2).Outcomes.BDI =  [ 35 10 13  0  0  0  0  0 ];
-Data(2).Outcomes.BAI =  [ 14  4 10  1  3  1  1  0 ];
-Data(2).Outcomes.EEGDay = [ 39 45 56 93];
+Data(kk).pID = {'AMa'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 16 23 30 37 45 55 65 ];
+Data(kk).Outcomes.BDI =  [ 40 24 14 15 16 14 16 15 ];
+Data(kk).Outcomes.BAI =  [ 14 10  3  3  5  4  4  3 ];
+Data(kk).Outcomes.EEGDay = [ 6 14 28 65 ];
+kk=kk+1;
 
-Data(3).pID = {'GMu'};
-Data(3).Sessions=[1 2 3 4];
-Data(3).Outcomes.Days = [  1 16 23 33 40 47 65 71 81 ]; 
-Data(3).Outcomes.BDI =  [ 38 17 11  9  8  4  3  2  1 ];
-Data(3).Outcomes.BAI =  [ 17  6  5  2  2  2  1  0  0 ];
-Data(3).Outcomes.EEGDay = [ 5 23 31 81 ];
+Data(kk).pID = {'AWa'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 45 53 59 66 73 81 93 ];
+Data(kk).Outcomes.BDI =  [ 35 10 13  0  0  0  0  0 ];
+Data(kk).Outcomes.BAI =  [ 14  4 10  1  3  1  1  0 ];
+Data(kk).Outcomes.EEGDay = [ 39 45 56 93];
+kk=kk+1;
 
-Data(4).pID = {'HLu'};
-Data(4).Sessions=[1 2 3 4];
-Data(4).Outcomes.Days = [ 1 58 66 72 84 91 101 114 ];
-Data(4).Outcomes.BDI = [ 29 29 21 24 28 18 12 11 ];
-Data(4).Outcomes.BAI = [ 4 5 3 2 4 2 1 2 ];
-Data(4).Outcomes.EEGDay = [ 51 60 71 114];
+Data(kk).pID = {'BKBr'};
+Data(kk).Sessions=[ 1 2 3 4 ];
+Data(kk).Outcomes.Days = [  1 63 70 78 85 92 101 121 ];
+Data(kk).Outcomes.BDI =  [ 20 19  7 18 18  8   9   8 ];
+Data(kk).Outcomes.BAI =  [  8  7  4  5  7  1   1   0 ];
+Data(kk).Outcomes.EEGDay = [ 51 63 84 121 ];
+kk=kk+1;
 
-Data(5).pID = {'JCa'};
-Data(5).Sessions=[1 2 3 4];
-Data(5).Outcomes.Days = [  1 24 31 54 61 71 78 93];
-Data(5).Outcomes.BDI =  [ 43 38 36 31 33 36 35 40 ];
-Data(5).Outcomes.BAI =  [ 45 37 35 40 30 39 43 42 ];
-Data(5).Outcomes.EEGDay = [ 10 22 51 93 ];
+Data(kk).pID = {'BPe'};         % needs updating
+Data(kk).Sessions=[ 1 2 3 4 ];
+Data(kk).Outcomes.Days = [  1  3 16 27 44 52 70 79 93 ];
+Data(kk).Outcomes.BDI =  [ 59 58 58 52 50 54 54 42 28 ];
+Data(kk).Outcomes.BAI =  [ 55 54 47 38 41 37 35 25 18 ];
+Data(kk).Outcomes.EEGDay = [ 9 14 41 93 ];
+kk=kk+1;
 
-Data(6).pID = {'MPe'}; 
-Data(6).Sessions=[2 3]; % fix structure!!
-Data(6).Outcomes.Days = [  1 13 20 27 35 42 49 58 ];
-Data(6).Outcomes.BDI =  [ 33 31 26 36 37 40 40 44 ];
-Data(6).Outcomes.BAI =  [  9 11  7  8 11 15 13 15 ];
-Data(6).Outcomes.EEGDay = [ 13 26 ];
+Data(kk).pID = {'CMi'};         % needs updating
+Data(kk).Sessions=[  ];
+Data(kk).Outcomes.Days = [  ];
+Data(kk).Outcomes.BDI =  [  ];
+Data(kk).Outcomes.BAI =  [  ];
+Data(kk).Outcomes.EEGDay = [  ];
+kk=kk+1;
 
-Data(7).pID = {'MWi'};
-Data(7).Sessions=[1 2 3 4];
-Data(7).Outcomes.Days = [  1 63 70 77 90 97 105 112 ];
-Data(7).Outcomes.BDI =  [ 38 32 22 14 14 11  11   6 ];
-Data(7).Outcomes.BAI =  [ 10  5  4  2  1  0   0   0 ];
-Data(7).Outcomes.EEGDay = [ 8 62 77 112 ];
-
-Data(8).pID = {'NTo'};
-Data(8).Sessions=[1 2 3 4];
-Data(8).Outcomes.Days = [  1 27 37 44 63 70 78 99 ];
-Data(8).Outcomes.BDI =  [ 41 25 32 15 18 15 12  8 ];
-Data(8).Outcomes.BAI =  [ 10 11 14  9  9  7  4  3 ];
-Data(8).Outcomes.EEGDay = [ 14 30 44 99 ];
-
-Data(9).pID = {'R-LGo'};
-Data(9).Sessions=[1 2 3 4];
-Data(9).Outcomes.Days = [  1 21 28 36 42 49 57 79 ];
-Data(9).Outcomes.BDI =  [ 46 26 10  4  2  2  2  1 ];
-Data(9).Outcomes.BAI =  [ 28 25 21 13  6  3  5  4 ];
-Data(9).Outcomes.EEGDay = [ 8 17 36 79 ];
-
-Data(10).pID = {'SHa'};
-Data(10).Sessions=[1 2 3];
-Data(10).Outcomes.Days = [  1 13 21 28 36 44 55 73 ];
-Data(10).Outcomes.BDI = [ 23 19 14 13 15  8 10  7 ];
-Data(10).Outcomes.BAI = [ 17 11  5  7  5  3  1  1 ];
-Data(10).Outcomes.EEGDay = [ 1 10 27 ];
-
-Data(11).pID = {'PCi'};
-Data(11).Sessions=[ 2 3 ];
-Data(11).Outcomes.BDI = [  ];
-Data(11).Outcomes.BAI = [  ];
-
-Data(12).pID = {'NSt'};
-Data(12).Sessions=[ 2 3 ];
-Data(12).Outcomes.BDI = [  ];
-Data(12).Outcomes.BAI = [  ];
-
-Data(13).pID = {'BKBr'};
-Data(13).Sessions=[ 3 ];
-Data(13).Outcomes.BDI = [  ];
-Data(13).Outcomes.BAI = [  ];
-
-Data(14).pID = {'SCo'};
-Data(14).Sessions=[ 3 ];
-Data(14).Outcomes.BDI = [  ];
-Data(14).Outcomes.BAI = [  ];
+Data(kk).pID = {'EBa'};        % needs updating
+Data(kk).Sessions=[ ];
+Data(kk).Outcomes.Days = [ ];
+Data(kk).Outcomes.BDI =  [  ];
+Data(kk).Outcomes.BAI =  [ ];
+Data(kk).Outcomes.EEGDay = [  ];
+kk=kk+1;
 
 
+Data(kk).pID = {'GMu'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 16 23 33 40 47 65 71 81 ]; 
+Data(kk).Outcomes.BDI =  [ 38 17 11  9  8  4  3  2  1 ];
+Data(kk).Outcomes.BAI =  [ 17  6  5  2  2  2  1  0  0 ];
+Data(kk).Outcomes.EEGDay = [ 5 23 31 81 ];
+kk=kk+1;
 
-sub = [1 3 7 9]; % subjects to look at, since not all are complete, and since some have bad sessions (see excel spreadsheet) due to loose reference on cap2
+Data(kk).pID = {'HLu'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [ 1 58 66 72 84 91 101 114 ];
+Data(kk).Outcomes.BDI = [ 29 29 21 24 28 18 12 11 ];
+Data(kk).Outcomes.BAI = [ 4 5 3 2 4 2 1 2 ];
+Data(kk).Outcomes.EEGDay = [ 51 60 71 114];
+kk=kk+1;
+
+Data(kk).pID = {'JCa'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 24 31 54 61 71 78 93];
+Data(kk).Outcomes.BDI =  [ 43 38 36 31 33 36 35 40 ];
+Data(kk).Outcomes.BAI =  [ 45 37 35 40 30 39 43 42 ];
+Data(kk).Outcomes.EEGDay = [ 10 22 51 93 ];
+kk=kk+1;
+
+Data(kk).pID = {'MPe'}; 
+Data(kk).Sessions=[2 3]; % fix structure!!
+Data(kk).Outcomes.Days = [  1 13 20 27 35 42 49 58 ];
+Data(kk).Outcomes.BDI =  [ 33 31 26 36 37 40 40 44 ];
+Data(kk).Outcomes.BAI =  [  9 11  7  8 11 15 13 15 ];
+Data(kk).Outcomes.EEGDay = [ 13 26 ];
+kk=kk+1;
+
+Data(kk).pID = {'Mru'};     % needs updating
+Data(kk).Sessions=[]; % fix structure!!
+Data(kk).Outcomes.Days = [   ];
+Data(kk).Outcomes.BDI =  [  ];
+Data(kk).Outcomes.BAI =  [ ];
+Data(kk).Outcomes.EEGDay = [  ];
+kk=kk+1;
+
+Data(kk).pID = {'MTa'};     % needs updating
+Data(kk).Sessions=[ ]; % fix structure!!
+Data(kk).Outcomes.Days = [  ];
+Data(kk).Outcomes.BDI =  [  ];
+Data(kk).Outcomes.BAI =  [   ];
+Data(kk).Outcomes.EEGDay = [  ];
+kk=kk+1;
+
+
+Data(kk).pID = {'MWh'};     % needs updating
+Data(kk).Sessions=[]; % fix structure!!
+Data(kk).Outcomes.Days = [   ];
+Data(kk).Outcomes.BDI =  [  ];
+Data(kk).Outcomes.BAI =  [   ];
+Data(kk).Outcomes.EEGDay = [  ];
+kk=kk+1;
+
+
+Data(kk).pID = {'MWi'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 63 70 77 90 97 105 112 ];
+Data(kk).Outcomes.BDI =  [ 38 32 22 14 14 11  11   6 ];
+Data(kk).Outcomes.BAI =  [ 10  5  4  2  1  0   0   0 ];
+Data(kk).Outcomes.EEGDay = [ 8 62 77 112 ];
+kk=kk+1;
+
+Data(kk).pID = {'NSt'};
+Data(kk).Sessions=[ 1 2 3 4 ];
+Data(kk).Outcomes.Days = [  1 66 74 81 92 99 106 117 ];
+Data(kk).Outcomes.BDI =  [ 36 10  9  3  6  6   4   2 ];
+Data(kk).Outcomes.BAI =  [ 20  8  5  4  4  9   4   3 ];
+Data(kk).Outcomes.EEGDay = [ 43 58 80 117 ];
+kk=kk+1;
+
+Data(kk).pID = {'NTo'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 27 37 44 63 70 78 99 ];
+Data(kk).Outcomes.BDI =  [ 41 25 32 15 18 15 12  8 ];
+Data(kk).Outcomes.BAI =  [ 10 11 14  9  9  7  4  3 ];
+Data(kk).Outcomes.EEGDay = [ 14 30 44 99 ];
+kk=kk+1;
+
+Data(kk).pID = {'PCi'};
+Data(kk).Sessions=[ 1 2 3 4 ];
+Data(kk).Outcomes.Days = [  1 22 29 36 43 50 57 72 ];
+Data(kk).Outcomes.BDI =  [ 50 23 20 19 20 31 30 38 ];
+Data(kk).Outcomes.BAI =  [ 39 24 20 17 16 19 19 26 ];
+Data(kk).Outcomes.EEGDay = [ 1 22 36 72];
+kk=kk+1;
+
+
+Data(kk).pID = {'R-LGo'};
+Data(kk).Sessions=[1 2 3 4];
+Data(kk).Outcomes.Days = [  1 21 28 36 42 49 57 79 ];
+Data(kk).Outcomes.BDI =  [ 46 26 10  4  2  2  2  1 ];
+Data(kk).Outcomes.BAI =  [ 28 25 21 13  6  3  5  4 ];
+Data(kk).Outcomes.EEGDay = [ 8 17 36 79 ];
+kk=kk+1;
+
+Data(kk).pID = {'RMe'};     % needs updating
+Data(kk).Sessions=[  ];
+Data(kk).Outcomes.Days = [   ];
+Data(kk).Outcomes.BDI =  [  ];
+Data(kk).Outcomes.BAI =  [  ];
+Data(kk).Outcomes.EEGDay = [  ];
+kk=kk+1;
+
+Data(kk).pID = {'SCo'};
+Data(kk).Sessions=[ 1 2 3 4 ];
+Data(kk).Outcomes.Days = [  1  3 16 27 44 52 70 79 93 ];
+Data(kk).Outcomes.BDI =  [ 59 58 58 52 50 54 54 42 28 ];
+Data(kk).Outcomes.BAI =  [ 55 54 47 38 41 37 35 25 18 ];
+Data(kk).Outcomes.EEGDay = [ 9 14 41 93 ];
+kk=kk+1;
+
+Data(kk).pID = {'SHa'};
+Data(kk).Sessions=[1 2 3];
+Data(kk).Outcomes.Days = [  1 13 21 28 36 44 55 73 ];
+Data(kk).Outcomes.BDI = [ 23 19 14 13 15  8 10  7 ];
+Data(kk).Outcomes.BAI = [ 17 11  5  7  5  3  1  1 ];
+Data(kk).Outcomes.EEGDay = [ 1 10 27 ];
+kk=kk+1;
+
+
+
+sub = [1 3 7 8 9]; % subjects to look at, since not all are complete, and since some have bad sessions (see excel spreadsheet) due to loose reference on cap2
 
 %% FEATURE CONSTRUCTION
 % Let's first construct features for each subject, we want to see how
@@ -239,15 +323,15 @@ for i = 1:length(sub)
     if (isfield(Data(sub(i)).Outcomes, 'EEGDay' ))
         c = Data(sub(i)).Outcomes.EEGDay(:) ;
         a = Data(sub(i)).Outcomes.Days(:);
-        b = Data(sub(i)).Outcomes.BDI(:) ;
-        d = Data(sub(i)).Outcomes.BAI(:) ;
-        r = interp1(a , b, c);
-        s = interp1(a , d, c);
+        b1 = Data(sub(i)).Outcomes.BDI(:) ;
+        b2 = Data(sub(i)).Outcomes.BAI(:) ;
+        r1 = interp1(a, b1, c);
+        r2 = interp1(a, b2, c);
     end          
         
     % vectors containing the BDI and BAI values on EEGDay 
-    BDI = cat(1, BDI , r);
-    BAI = cat(1, BAI , s);
+    BDI = cat(1, BDI , r1);
+    BAI = cat(1, BAI , r2);
         
 end;  
 
@@ -261,7 +345,7 @@ end
 
 
 [d_corr_values d_corr_ind] = sort(crl_depression);
-[a_corr_values a_corr_ind] = sort(crl_anxiety);
+[a_corr_values a_corr_ind] = sort(crl_depression);
 
 clearvars -except Features Data mRootDir features sub BDI BAI d_corr_values d_corr_ind a_corr_values a_corr_ind; 
 
