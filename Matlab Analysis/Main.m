e7ef82f7d614951fb8cadcf21489857c3417f464
mmicr001 <mmicr001@odu.edu>
2016-06-27 11:56:20 -0400

    Add files via upload

--- Matlab Analysis/Main.m
+++ Matlab Analysis/Main.m
@@ -22,7 +22,7 @@ mRootDir = 'C:\Users\Mike\Google Drive\EVMS-PTSD\';
 % Patient Identification
 kk=1;
 
-Data(kk).pID = {'PT01'};
+Data(kk).pID = {'PT01'};            %improved
 Data(kk).Sessions=             [  1  2  3 ];
 Data(kk).Outcomes.Checklist =  [ 77 22 17 ];
 Data(kk).Outcomes.PTSDscale =  [ 27  6  2 ];
@@ -31,7 +31,7 @@ Data(kk).Outcomes.BAI =        [ 38  0  0 ];
 kk=kk+1;
 
 
-Data(kk).pID = {'PT02'};
+Data(kk).pID = {'PT02'};            %didnt improve
 Data(kk).Sessions=             [  1  2  3 ];
 Data(kk).Outcomes.Checklist =  [ 69 59 67 ];
 Data(kk).Outcomes.PTSDscale =  [ 30 17 24 ];
@@ -39,7 +39,7 @@ Data(kk).Outcomes.BDI =        [ 38 25 27 ];
 Data(kk).Outcomes.BAI =        [ 32 25 47 ];
 kk=kk+1;
 
-Data(kk).pID = {'PT03'};
+Data(kk).pID = {'PT03'};        %improved
 Data(kk).Sessions=             [  1  2  3 ];
 Data(kk).Outcomes.Checklist =  [ 61 38 38 ];
 Data(kk).Outcomes.PTSDscale =  [ 27 15 17 ];
@@ -47,7 +47,7 @@ Data(kk).Outcomes.BDI =        [ 29  4  4 ];
 Data(kk).Outcomes.BAI =        [ 14  7 11 ];
 kk=kk+1;
 
-Data(kk).pID = {'PT04'};
+Data(kk).pID = {'PT04'};            %improved
 Data(kk).Sessions=             [  1  2  3 ];
 Data(kk).Outcomes.Checklist =  [ 48 33 30 ];
 Data(kk).Outcomes.PTSDscale =  [ 22 17 14 ];
@@ -55,7 +55,7 @@ Data(kk).Outcomes.BDI =        [ 24  6  6 ];
 Data(kk).Outcomes.BAI =        [ 13  9 10 ];
 kk=kk+1;
 
-Data(kk).pID = {'PT05'};
+Data(kk).pID = {'PT05'};        %improved
 Data(kk).Sessions=             [  1  2  3 ];
 Data(kk).Outcomes.Checklist =  [ 56 23 19 ];
 Data(kk).Outcomes.PTSDscale =  [ 15  5  1 ];
@@ -63,7 +63,7 @@ Data(kk).Outcomes.BDI =        [ 27  7  4 ];
 Data(kk).Outcomes.BAI =        [ 15  6  0 ];
 kk=kk+1;
 
-Data(kk).pID = {'PT06'};
+Data(kk).pID = {'PT06'};                %didnt improve
 Data(kk).Sessions=             [  1  2   ];
 Data(kk).Outcomes.Checklist =  [ 58 42 ];% 33 ];
 Data(kk).Outcomes.PTSDscale =  [ 20 17 ];% 16 ];
@@ -80,7 +80,7 @@ kk=kk+1;
 % Data(kk).Outcomes.BAI =        [ 51 ];%21 10 ];
 % kk=kk+1;
 
-Data(kk).pID = {'PT08'};
+Data(kk).pID = {'PT08'};                %didnt improve
 Data(kk).Sessions=             [  1  2 ];
 Data(kk).Outcomes.Checklist =  [ 70 73 ];%64 ];
 Data(kk).Outcomes.PTSDscale =  [ 28 25 ];% 23 ];
@@ -88,7 +88,7 @@ Data(kk).Outcomes.BDI =        [ 44 35 ];%32 ];
 Data(kk).Outcomes.BAI =        [ 35 27 ];% 23 ];
 kk=kk+1;
 
-Data(kk).pID = {'PT09'};
+Data(kk).pID = {'PT09'};            %improved
 Data(kk).Sessions=             [  1  2  3 ];
 Data(kk).Outcomes.Checklist =  [ 66 48 27 ];
 Data(kk).Outcomes.PTSDscale =  [ 24 17  8 ];
@@ -96,8 +96,8 @@ Data(kk).Outcomes.BDI =        [ 33  2  4 ];
 Data(kk).Outcomes.BAI =        [ 35  9  6 ];
 kk=kk+1;
 
-sub = [ 1 : 8 ]; % subjects to look at, since not all are complete, and since some have bad sessions (see excel spreadsheet) due to loose reference on cap2
-
+% sub = [ 1 :8 ]; % subjects to look at, since not all are complete, and since some have bad sessions (see excel spreadsheet) due to loose reference on cap2
+sub =[1 3 4 5 8 ];
 %% FEATURE CONSTRUCTION
 % Let's first construct features for each subject, we want to see how
 % they vary over the course of the treatment, and if they vary in
@@ -121,19 +121,22 @@ for i = sub
            
             [ sig,st,prm ] = load_bcidat( [char(mDir) char(mFiles(1).name)],'-calibrated' );
             [ sig,st,prm ] = ArtifactRejection( sig,st,prm );
-            Features{cntr}.Open{j,1} = GetRestingFeatures( sig, st, prm );
+%             Features{cntr}.Open{j,1} = GetRestingFeatures( sig, st, prm );
+            Features{cntr}.Open{j,1} = RestingFeaturesKemp( sig, st, prm );
             fprintf(1,'.');             
             
             [ sig,~,prm ] = load_bcidat( [char(mDir) char(mFiles(2).name)],'-calibrated' );
             [ sig,st,prm ] = ArtifactRejection( sig,st,prm );
-            Features{cntr}.Closed{j,1} = GetRestingFeatures( sig, st, prm );
+%             Features{cntr}.Closed{j,1} = GetRestingFeatures( sig, st, prm );
+            Features{cntr}.Closed{j,1} = RestingFeaturesKemp( sig, st, prm );
             fprintf(1,'.');            
            
         end
     end        
     fprintf(1,'done\n');
     cntr=cntr+1;
-end; clearvars -except Features Data mRootDir sub;
+end;
+clearvars -except Features Data mRootDir sub;
 
 
 
@@ -144,20 +147,20 @@ end; clearvars -except Features Data mRootDir sub;
 % let's just stick these in order [open closed p3], so (43+43+15)=101
 % measures over 4 sessions, still keep as cell arrays for subject?
 % replace missing features with Not-a-Number
-
+% 
 maxSessions = 3;           % at most 4 sessions
 for i = 1:length(Features) % for each subject                           
     o = Features{i}.Open; % for eyes open
     mMaxSize = 0;
-    for j = 1:maxSessions 
+    for j = 1:maxSessions
         if( j < length(o) )
-            ms= length( cell2mat( o(j) ));
+             ms= length( cell2mat( o(j) ));
             if( ms > mMaxSize )
-                mMaxSize = ms;
+%                 mMaxSize = ms;
             end
         end        
     end
-    for j = 1:maxSessions
+    for j =1 : maxSessions
        if( j<=length(o) )
            if isempty( cell2mat( o(j) ) )
                o{j} = NaN(1,mMaxSize);
@@ -169,11 +172,11 @@ for i = 1:length(Features) % for each subject
     o = cell2mat( o ); % for eyes closed                           
     c = Features{i}.Closed;
     mMaxSize = 0;
-    for j = 1:maxSessions 
+    for j = 1:maxSessions
         if( j < length(c) )
             ms= length( cell2mat( c(j) ));
             if( ms > mMaxSize )
-                mMaxSize = ms;
+%                 mMaxSize = ms;
             end
         end        
     end
@@ -194,46 +197,70 @@ features = features';
 features = cell2mat( features );
 
 
-%% NOW SEE WHAT FEATURES FOLLOW TRENDS ACROSS SUBJECTS
-%  E.G., CONSISTENTLY INCREASE OR DECREASE THROUGH TREATMENT SESSIONS
+% %% NOW SEE WHAT FEATURES FOLLOW TRENDS ACROSS SUBJECTS
+% %  E.G., CONSISTENTLY INCREASE OR DECREASE THROUGH TREATMENT SESSIONS
+% 
+% % we create vectors containing the BDI, BAI, checklist and PTSD scale values 
+% BAI = [];
+% BDI = [];
+% Checklist = [];
+% PTSDscale = [];
+% 
+% for i = 1:length(sub)
+%         
+%     % vectors containing the BDI and BAI values on EEGDay 
+%     BDI = cat(1, BDI , Data(i).Outcomes.BDI');
+%     BAI = cat(1, BAI , Data(i).Outcomes.BAI');
+%     Checklist = cat(1, Checklist , Data(i).Outcomes.Checklist');
+%     PTSDscale = cat(1, PTSDscale , Data(i).Outcomes.PTSDscale');
+%         
+% end;  
+% 
+% %compute correlation
+% corr_BDI = zeros (1, size(features,2));  
+% corr_BAI = zeros (1, size(features,2));  
+% corr_Checklist = zeros (1, size(features,2));   
+% corr_PTSDscale = zeros (1, size(features,2));   
+% 
+% for i = 1 : size(features,2)
+%         corr_BDI(i) = nancorr( BDI , features(:,i));
+%         corr_BAI(i) = nancorr( BAI , features(:,i));
+%         corr_Checklist(i) = nancorr( Checklist , features(:,i));
+%         corr_PTSDscale(i) = nancorr( PTSDscale , features(:,i));
+% end
+% 
+% sq_BDI = corr_BDI.^2;
+% sq_BAI = corr_BAI.^2;
+% sq_Checklist = corr_Checklist.^2;
+% sq_PTSDscale = corr_PTSDscale.^2;
+% 
+% 
+% [BDI_corrValues BDI_corrInd] = sort(sq_BDI);
+% [BAI_corrValues BAI_corrInd] = sort(sq_BAI);
+% [Checklist_corrValues Checklist_corrInd] = sort(sq_Checklist);
+% [PTSDscale_corrValues PTSDscale_corrInd] = sort(sq_PTSDscale);
+% 
+% [BDI_rValues BDI_rInd] = sort(corr_BDI);
+% [BAI_rValues BAI_rInd] = sort(corr_BAI);
+% [Checklist_rValues Checklist_rInd] = sort(corr_Checklist);
+% [PTSDscale_rValues PTSDscale_rInd] = sort(corr_PTSDscale);
 
-% we create vectors containing the BDI, BAI, checklist and PTSD scale values 
-BAI = [];
-BDI = [];
-Checklist = [];
-PTSDscale = [];
+%clearvars -except Features Data mRootDir features sub BDI BAI d_corr_values d_corr_ind a_corr_values a_corr_ind; 
 
-for i = 1:length(sub)
-        
-    % vectors containing the BDI and BAI values on EEGDay 
-    BDI = cat(1, BDI , Data(i).Outcomes.BDI');
-    BAI = cat(1, BAI , Data(i).Outcomes.BAI');
-    Checklist = cat(1, Checklist , Data(i).Outcomes.Checklist');
-    PTSDscale = cat(1, PTSDscale , Data(i).Outcomes.PTSDscale');
-        
-end;  
 
-%compute correlation
-corr_BDI = zeros (1, size(features,2));  
-corr_BAI = zeros (1, size(features,2));  
-corr_Checklist = zeros (1, size(features,2));   
-corr_PTSDscale = zeros (1, size(features,2));   
 
-for i = 1 : size(features,2)
-        corr_BDI(i) = nancorr( BDI , features(:,i));
-        corr_BAI(i) = nancorr( BAI , features(:,i));
-        corr_Checklist(i) = nancorr( Checklist , features(:,i));
-        corr_PTSDscale(i) = nancorr( PTSDscale , features(:,i));
+before = zeros( size(Features,2) , 2 * length(Features{1}.Open{1}) );
+after =  zeros( size(Features,2) , 2 * length(Features{1}.Open{1}) );
+
+for i = 1 : size(Features,2)
+    before(i,:) = [ Features{i}.Open{1} ; Features{i}.Closed{1} ];  
+    after(i,:) = [ Features{i}.Open{end} ;  Features{i}.Closed{end} ];
 end
 
 
-[BDI_corrValues BDI_corrInd] = sort(corr_BDI);
-[BAI_corrValues BAI_corrInd] = sort(corr_BAI);
-[Checklist_corrValues Checklist_corrInd] = sort(corr_Checklist);
-[PTSDscale_corrValues PTSDscale_corrInd] = sort(corr_PTSDscale);
-
-clearvars -except Features Data mRootDir features sub BDI BAI d_corr_values d_corr_ind a_corr_values a_corr_ind; 
-
+for i = 1 : size(before,2)
+    p(i) = signrank( before(:,i), after(:,i) );
+end
 
 
 
